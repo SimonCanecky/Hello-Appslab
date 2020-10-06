4e1863e3ca4f3078ccc90d25ca7ef71a00bd8073
ŠimonČanecký <simoncanecky1@gmail.com>
2020-10-06 09:26:59 +0200

    Student class additional details vytlaci sa cisla studentov, telefonne cisla a adresy dvoch studentov

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,14 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Student John = new Student( 2, 123456, "Legionárska 1, 01001 Žilina" );
+        Student Sam = new Student( 5, 456789, "Legionárska 2, 01001 Žilina" );
+
+        System.out.println( John.studentNumber + ", " + John.studentPhone + ", " + John.Address);
+        System.out.println( Sam.studentNumber + ", " + Sam.studentPhone + ", " + Sam.Address);
+
+
     }
 }
+
+
---
+++ src/com/appslab/Student.java
@@ -0,0 +1,14 @@
+package com.appslab;
+
+public class Student {
+    public int studentNumber;
+    public int studentPhone;
+    public String Address;
+    public Student(int number, int phone, String address){
+        studentNumber = number;
+        studentPhone = phone;
+        Address = address;
+
+    }
+
+}
