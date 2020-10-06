41d8d437e0be4f241494f8cd8d7bdaca8dc9ca09
ŠimonČanecký <simoncanecky1@gmail.com>
2020-10-06 09:55:26 +0200

    Get Sum of People's Budget funkcia vezme pole s objektmi a vrati sucet rozpoctov ludi

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,13 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Person John = new Person("John", 21, 23000);
+        Person Steve = new Person("Steve", 32, 40000);
+        Person Martin = new Person("Martin", 16, 2700);
+
+        int sum;
+        sum = John.personRozpocet + Steve.personRozpocet + Martin.personRozpocet;
+
+        System.out.println(sum);
     }
 }
---
+++ src/com/appslab/Person.java
@@ -0,0 +1,13 @@
+package com.appslab;
+
+public class Person {
+    public String personName;
+    public int personVek;
+    public int personRozpocet;
+
+    public Person(String name, int vek, int rozpocet){
+       personName = name;
+       personVek = vek;
+       personRozpocet = rozpocet;
+    }
+}
