9419df89e600eb540def8784122f5fa10564bd2c
ŠimonČanecký <simoncanecky1@gmail.com>
2020-10-06 09:03:21 +0200

    Slice of Pie funkcia urci ci je mozne spravodlivo rozdelit kolac

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,12 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        System.out.println(equalSlices(11, 5,2));
+        System.out.println("(11, 5, 2)");
     }
+    public static boolean equalSlices(int totalSlices, int noRecipients, int slicesEach) {
+        return noRecipients * slicesEach <= totalSlices;
+    }
+
 }
