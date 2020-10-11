62593b7a5eb8234a60b3c01c73e5fa283b6d9658
SimonCanecky <simoncanecky1@gmail.com>
2020-10-11 19:13:52 +0200

    Is the String a Palindrome? funkcia skontroluje ci ide o palindrome

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -2,7 +2,19 @@ package com.appslab;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+    public static void main (String[] args)
+    {
+        System.out.println(isPalindrome("mom"));
+
+    }
+    public static boolean isPalindrome(String slovo){
+        String reverse = new StringBuffer(slovo).reverse().toString();
+
+        if (slovo.equals(reverse))
+            return true;
+
+        else
+            return false;
     }
 }
