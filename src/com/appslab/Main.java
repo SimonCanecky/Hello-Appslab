121ebddb79994313bfebf200e6cf44d927bed7d1
SimonCanecky <simoncanecky1@gmail.com>
2020-10-13 14:27:47 +0200

    Difference of Max and Min Numbers in Array funkciu zoberie pole a vrati rozdiel medzi najväcsim a najmensim cislom

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,35 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        int[] list = {10, 4, 1, 4, -10, -50, 32, 21};
+
+        int max = getMax(list);
+        System.out.println("Maximum Value is: "+max);
+
+        int min = getMin(list);
+        System.out.println("Minimum Value is: "+min);
+
+        int vysledok = max - min;
+        System.out.println("Difference between the biggest and smallest numbers is: "+vysledok);
+    }
+
+    public static int getMax(int[] inputArray){
+        int maxValue = inputArray[0];
+        for(int i=1;i < inputArray.length;i++){
+            if(inputArray[i] > maxValue){
+                maxValue = inputArray[i];
+            }
+        }
+        return maxValue;
+    }
+
+    public static int getMin(int[] inputArray){
+        int minValue = inputArray[0];
+        for(int i=1;i<inputArray.length;i++){
+            if(inputArray[i] < minValue){
+                minValue = inputArray[i];
+            }
+        }
+        return minValue;
     }
 }
